50356bdbc9838059b985cc6b337e7083a86c08cc
KarlT <43105516+KarlTrollsaas@users.noreply.github.com>
2020-04-02 11:46:51 +0200

    Update LT-Install.sh

--- LT-Install.sh
+++ LT-Install.sh
@@ -1,18 +1,21 @@
 #! /usr/bin/env bash
 # comment: use command ./LT-Install.sh
 
+# Install a foreign function interface, so code written in one language can call code made in other languages.
 echo "*************************"
 echo "* Installing libffi-dev *"
 echo "*************************"
 sudo apt-get install libffi-dev
 echo
 
+# Install a spesific crypthography version for compatibility reasons.
 echo "*****************************************"
 echo "* Installing cryptography version 2.1.4 *"
 echo "*****************************************"
 sudo pip install cryptography==2.1.4
 echo
 
+# Install an extention to use for voice interface to Alexa.
 echo "************************"
 echo "* Installing flask-ask *"
 echo "************************"
