f1ccf005be092f4ccf6fe6be963e6f376fda9c11
Justtinas <bulovas@gmail.com>
2020-04-03 12:13:29 +0200

    Update light.py

--- Code/light.py
+++ Code/light.py
@@ -3,23 +3,29 @@ from flask_ask import Ask, statement, question, session
 import RPi.GPIO as GPIO
 import time
 
+#GPIO setup
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
 GPIO.setup(18, GPIO.OUT, initial=GPIO.LOW)
 
+# Load the driver and set it to "display"
+# If you use something from the driver library use the "display." prefix first
 app = Flask(__name__)
 ask = Ask(app, '/')
 
+#starting web application
 @app.route('/')
 def homepage():
     return "Hi"
 
+#start question when the skill is trigered by Alexa
 @ask.launch
 def start_skill():
     message = "Do you want to turn the light on or off?"
     return question(message)
 
+#function to turn the light on
 @ask.intent('LightOn')
 def lightOn():
     GPIO.output(18, GPIO.HIGH)
@@ -27,6 +33,7 @@ def lightOn():
     light = "Ok, the light is now on"
     return statement(light)
 
+#function to turn the light off
 @ask.intent('LightOff')
 def lightOff():
     GPIO.output(18, GPIO.LOW)
